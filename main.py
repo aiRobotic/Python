a5f1ceb4fe0fa1ad3127d6135a604c8b5e12f902
aiRobotic <alisenguel@yahooo.com>
2021-09-15 21:27:35 +0100

    multiple screens

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.lang import Builder 
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
+import json,glob,random
 from datetime import datetime
+from pathlib import Path
 
 # Replace this with
 # current version
@@ -14,6 +15,15 @@ Builder.load_file('design.kv')
 class LoginScreen(Screen):
     def sign_up (self):
         self.manager.current = "Sign_Up_Screen"
+    
+    def login(self,uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+
+        if uname in users and users[uname]['password']== pword:
+            self.manager.current = "Login_Success"
+        else:
+            self.ids.incorrect_input.text = "Incorrect Username or Password \n                  Try again!" 
 
 class SignUpScreen(Screen):
     def add_user(self, uname, pword):
@@ -21,10 +31,29 @@ class SignUpScreen(Screen):
             users = json.load(file)
         users[uname] ={'username':uname,'password':pword,
         'created':datetime.now().strftime("%Y-%M-%D-%H-%S")}
-        with open ("users.joson","w") as file:
+        with open ("users.json","w") as file:
             json.dump(users,file)
+        self.manager.current = "Sign_Up_Screen_Success"
 
+class SignUpScreenSuccess(Screen):
+    def go_to_login(self):
+        self.manager.current = "Login_Screen"
 
+class LoginSuccess(Screen):
+    def logout(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "Login_Screen"
+    
+    def get_feeling(self, feel):
+        feel = feel.lower()
+        files = glob.glob("quotes/*.txt")
+        feelings = [Path(filename).stem for filename in files]
+        if feel in feelings:
+            with open(f"quotes/{feel}.txt") as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling"    
 class RootWidget (ScreenManager):
     def build(self):
         return LoginScreen()
